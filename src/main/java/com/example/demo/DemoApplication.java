c2678278d04462a301b9318883d5a194826ae576
tsahaca <tanmay.x.saha@gmail.com>
2021-04-26 11:56:05 -0700

    removed unused import

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class DemoApplication  implements CommandLineRunner {
